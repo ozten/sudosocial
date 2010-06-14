21515f5daece1a166d28c33628e5a77c65e7e483
Austin King <shout@ozten.com>
2010-06-14 09:30:28 -0700

    Adding feedacache for performance

--- cron/feeder.py
+++ cron/feeder.py
@@ -18,6 +18,9 @@ import django.utils.encoding
 import django.utils.hashcompat as hashcompat
 import feedparser
 import jsonpickle
+if config.cache:
+    import shelve
+    from feedcache import cache
 
 import lifestream.models
 
@@ -31,49 +34,66 @@ def cron_fetch_feeds():
     start = time.time()
     new_entry_count = 0
     feeds = lifestream.models.Feed.objects.all()
-    for feed in feeds:
+    
+    if config.cache:
+        storage = shelve.open('.feedcache')
         
-        stream = feedparser.parse(feed.url)
-        if 1 == stream.bozo and 'bozo_exception' in stream.keys():
-            log.error('Unable to fetch %s Exception: %s',
-                      feed.url, stream.bozo_exception)
-        else:
-            log.info('Processing %s', feed.url)
-            if 'feed' in stream and 'title' in stream.feed:
-                log.info('Processing title: %s', stream.feed.title)
-            if 'feed' in stream and 'title' in stream.feed and feed.title != stream.feed.title:
-                feed.title = stream.feed.title
-                try:
-                    feed.save()
-                except Exception, x:
-                    log.error("Unable to update feed title from=%s to=%s" % (feed.title, stream.title))
-                    log.error(x)
+    try:
+        if config.cache:
+            fc = cache.Cache(storage)
+        for feed in feeds:
+            if config.cache:
+                stream = fc.fetch(feed.url)
             else:
-                if (not 'feed' in stream) or (not 'title' in stream.feed):
-                    log.warn("Feed doesn't have a title property")
-                    log.info(stream)
-            for entry in stream.entries:
-                try:
-                    json_entry = jsonpickle.encode(entry)
-                    entry_guid = ''
-                    if 'guid' in entry:
-                        entry_guid = entry.guid
-                    else:
-                        entry_guid = hashcompat.md5_constructor(
-                            django.utils.encoding.smart_str(
-                                json_entry)).hexdigest()
-                    yr, mon, d, hr, min, sec = entry.updated_parsed[:-3]
-                    last_publication = datetime.datetime(yr, mon, d, hr, min, sec)
-                    new_entry = lifestream.models.Entry(feed=feed, guid=entry_guid, raw=json_entry,
-                                                       visible=True, last_published_date=last_publication)
+                stream = feedparser.parse(feed.url)                            
+            #stream = feedparser.parse(feed.url)
+            if 1 == stream.bozo and 'bozo_exception' in stream.keys():
+                log.error('Unable to fetch %s Exception: %s',
+                          feed.url, stream.bozo_exception)
+            else:
+                log.info('Processing %s', feed.url)
+                if 'feed' in stream and 'title' in stream.feed:
+                    log.info('Processing title: %s', stream.feed.title)
+                if 'feed' in stream and 'title' in stream.feed and feed.title != stream.feed.title:
+                    feed.title = stream.feed.title
                     try:
-                        new_entry.save()
-                        new_entry_count += 1
-                    except IntegrityError, e:
-                        log.info('Skipping duplicate entry %s, caught error: %s', entry_guid, e)
-                except:
-                    # TODO monitor exceptions here, for now this keeps us from a dead cron
-                    pass
+                        feed.save()
+                    except Exception, x:
+                        log.error("Unable to update feed title from=%s to=%s" % (feed.title, stream.title))
+                        log.error(x)
+                else:
+                    if (not 'feed' in stream) or (not 'title' in stream.feed):
+                        log.warn("Feed doesn't have a title property")
+                        log.info(stream)
+                for entry in stream.entries:
+                    try:
+                        json_entry = jsonpickle.encode(entry)
+                        entry_guid = ''
+                        if 'guid' in entry:
+                            entry_guid = entry.guid
+                        else:
+                            entry_guid = hashcompat.md5_constructor(
+                                django.utils.encoding.smart_str(
+                                    json_entry)).hexdigest()
+                        yr, mon, d, hr, min, sec = entry.updated_parsed[:-3]
+                        last_publication = datetime.datetime(yr, mon, d, hr, min, sec)
+                        new_entry = lifestream.models.Entry(feed=feed, guid=entry_guid, raw=json_entry,
+                                                           visible=True, last_published_date=last_publication)
+                        try:
+                            new_entry.save()
+                            new_entry_count += 1
+                        except IntegrityError, e:
+                            pass
+                            #log.info('Skipping duplicate entry %s, caught error: %s', entry_guid, e)
+                    except:
+                        # TODO monitor exceptions here, for now this keeps us from a dead cron
+                        pass            
+    finally:
+        if config.cache:
+            log.info("Done with cache")
+            storage.close()
+        else:
+            log.info("Without caching")
     log.info("Finished run in %f seconds" % (time.time() - start))  
     return 'Finished importing %d items' % new_entry_count
 
