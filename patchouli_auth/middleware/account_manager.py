102eca2a0d99cbf08e3c99a570c337c9569ddf69
Austin King <shout@ozten.com>
2010-07-16 08:30:16 -0700

    Removing hardcoded hostname from account management middlewear

--- patchouli_auth/middleware/account_manager.py
+++ patchouli_auth/middleware/account_manager.py
@@ -1,4 +1,4 @@
 class AccountManagerMiddleware(object):
     def process_response(self, request, response):        
-        response['Link'] = "<http://patchouli.ubuntu/static/.well-known/host-meta>; rel=\"account-mgmt\""
+        response['Link'] = "<http://%s:%s/static/.well-known/host-meta>; rel=\"account-mgmt\"" % (request.META['HTTP_HOST'], request.META['SERVER_PORT'])
         return response
